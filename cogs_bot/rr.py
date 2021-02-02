e28081f5e2f2ba3d6ca095006a6489f8f61dd7f7
Shuana <61404361+shuanaongithub@users.noreply.github.com>
2021-02-02 15:39:57 +0000

    mhm

--- cogs_bot/rr.py
+++ cogs_bot/rr.py
@@ -14,24 +14,24 @@ from discord.ext import commands
 class ReactionRoles(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.isthicc = bot.get_guild(739510335949635736)
+        self.guild = bot.get_guild(739510335949635736)
 
 #
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 #
-    @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload):
+    @commands.Cog.listener() # Make the bot listen for a function
+    async def on_raw_reaction_add(self, payload): # This is the function the bot is listening for
         if payload.message_id == 804177369584631810: # change
             if str(payload.emoji) == "🔔":
-                role = self.isthicc.get_role(804161887704973323)
-                await payload.member.add_roles(role)
+                role = self.guild.get_role(804161887704973323) # Fetch a discord.Role from our self.guild
+                await payload.member.add_roles(role) # Give the payloads member the role(The member who reacted)
             elif str(payload.emoji) == "📊":
-                role = self.isthicc.get_role(804161985679720458)
+                role = self.guild.get_role(804161985679720458)
                 await payload.member.add_roles(role)
             elif str(payload.emoji) == "💻":
-                role = self.isthicc.get_role(804161946004881448)
+                role = self.guild.get_role(804161946004881448)
                 await payload.member.add_roles(role)
 #
 #
@@ -42,13 +42,13 @@ class ReactionRoles(commands.Cog):
     async def on_raw_reaction_remove(self, payload):
         if payload.message_id == 804177369584631810: # change
             if str(payload.emoji) == "🔔":
-                role = self.isthicc.get_role(804161887704973323)
-                await payload.member.remove_roles(role)
+                role = self.guild.get_role(804161887704973323)
+                await payload.member.remove_roles(role) # Remove the role from the payloads member
             elif str(payload.emoji) == "📊":
-                role = self.isthicc.get_role(804161985679720458)
+                role = self.guild.get_role(804161985679720458)
                 await payload.member.remove_roles(role)
             elif str(payload.emoji) == "💻":
-                role = self.isthicc.get_role(804161946004881448)
+                role = self.guild.get_role(804161946004881448)
                 await payload.member.remove_roles(role)
 
 #
