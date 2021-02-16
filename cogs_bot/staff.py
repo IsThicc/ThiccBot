c0169e407595627afe482e7862e2532aa2f726ba
Mrmagicpie <realmrmagicpie@gmail.com>
2021-02-15 19:35:45 -0500

    Update staff.py

--- cogs_bot/staff.py
+++ cogs_bot/staff.py
@@ -56,41 +56,40 @@ class staff_cog(commands.Cog):
                     icon_url=self.bot.user.avatar_url,
                     text="IsThicc Staff"
                 ))
-            try:
 
-                discord_member = discord.utils.get(ctx.guild.members, name=member.split("#")[0])
+            discord_member = discord.utils.get(ctx.guild.members, name=member.split("#")[0])
 
-                if discord_member is not None:
-                    msg = await ctx.send(embed=em(
-                        title=f"Attempting to view: {discord_member.display_name}",
-                        colour=discord.Colour.green(),
-                        timestamp=datetime.utcnow()
-                    ).set_footer(
-                        icon_url=self.bot.user.avatar_url,
-                        text="IsThicc Staff"
-                    ))
-                    member = discord_member.id
+            if discord_member is not None:
+                msg = await ctx.send(embed=em(
+                    title=f"Attempting to view: {discord_member.display_name}",
+                    colour=discord.Colour.green(),
+                    timestamp=datetime.utcnow()
+                ).set_footer(
+                    icon_url=self.bot.user.avatar_url,
+                    text="IsThicc Staff"
+                ))
+                member = discord_member.id
 
-                elif member.startswith("<@"):
-                    member = member.replace("<@!", "").replace(">", "")
-                    msg = await ctx.send(embed=em(
-                        title=f"Attempting to view: {member}",
-                        colour=discord.Colour.green(),
-                        timestamp=datetime.utcnow()
-                    ).set_footer(
-                        icon_url=self.bot.user.avatar_url,
-                        text="IsThicc Staff"
-                    ))
+            elif member.startswith("<@"):
+                member = member.replace("<@!", "").replace(">", "")
+                msg = await ctx.send(embed=em(
+                    title=f"Attempting to view: {member}",
+                    colour=discord.Colour.green(),
+                    timestamp=datetime.utcnow()
+                ).set_footer(
+                    icon_url=self.bot.user.avatar_url,
+                    text="IsThicc Staff"
+                ))
 
-                else:
-                    msg = await ctx.send(embed=em(
-                        title=f"Attempting to view: {member}",
-                        colour=discord.Colour.green(),
-                        timestamp=datetime.utcnow()
-                    ).set_footer(
-                        icon_url=self.bot.user.avatar_url,
-                        text="IsThicc Staff"
-                    ))
+            else:
+                msg = await ctx.send(embed=em(
+                    title=f"Attempting to view: {member}",
+                    colour=discord.Colour.green(),
+                    timestamp=datetime.utcnow()
+                ).set_footer(
+                    icon_url=self.bot.user.avatar_url,
+                    text="IsThicc Staff"
+                ))
 
                 request = await self.session.get(f"http://10.42.10.4:5000/staff/{member}")
                 code = request.status
@@ -126,81 +125,79 @@ class staff_cog(commands.Cog):
                     staff.add_field(
                         name="VPN IP",
                         value=response['details']['ip']
-                    )
+                )
+                staff.add_field(
+                    name="Discord User",
+                    value=self.bot.get_user(response['details']['discord_id']).mention
+                )
+                staff.add_field(
+                    name="GitHub Access",
+                    value="\n".join(github)
+                )
+                staff.add_field(
+                    name="GitHub ID",
+                    value=response['details']['github_id']
+                )
+                staff.add_field(
+                    name="GitHub Username",
+                    value=f"[{response['details']['github_username']}](https://github.com/{response['details']['github_username']})"
+                )
+
+                if github_code == 200:
+
+                    github_response = await github_r.json()
+
+                    if github_response['twitter_username'] is not None: twitter = github_response['twitter_username']
+                    else: twitter = "No Twitter on GitHub!"
                     staff.add_field(
-                        name="Discord User",
-                        value=self.bot.get_user(response['details']['discord_id']).mention
+                        name="Twitter",
+                        value=twitter
                     )
+
+                    if github_response['hireable'] == True: hire = "Yes!"
+                    else: hire = "Not currently!"
                     staff.add_field(
-                        name="GitHub Access",
-                        value="\n".join(github)
+                        name="Open to commissions?",
+                        value=hire
                     )
+
+                    if github_response['blog'] is None or len(github_response['blog']) == 0 or github_response['blog'] == "":
+                        site = "No Website on GitHub!"
+                    else: site = github_response['blog']
                     staff.add_field(
-                        name="GitHub ID",
-                        value=response['details']['github_id']
+                        name="Website",
+                        value=site
                     )
+
+                    if github_response['company'] is not None:
+                        if github_response['company'].startswith("@"):
+                            try: company = f'[{github_response["company"].replace("@", "")}](https://github.com/{github_response["company"].replace("@", "")})'
+                            except: company = github_response['company']
+                        else:
+                            company = github_response['company']
+                    else: company = "No Company on GitHub!"
                     staff.add_field(
-                        name="GitHub Username",
-                        value=f"[{response['details']['github_username']}](https://github.com/{response['details']['github_username']})"
+                        name="Company",
+                        value=company
                     )
 
-                    if github_code == 200:
+                staff.add_field(
+                    name="Position",
+                    value="\n".join(positions)
+                )
+                staff.add_field(
+                    name="System Access",
+                    value="\n".join(sysaccess)
+                )
 
-                        github_response = await github_r.json()
+                staff.set_footer(
+                    icon_url=self.bot.user.avatar_url,
+                    text="IsThicc Staff"
+                )
+                request.close()
+                github_r.close()
 
-                        if github_response['twitter_username'] is not None:
-                            twitter = github_response['twitter_username']
-                        else: twitter = "No Twitter on GitHub!"
-                        staff.add_field(
-                            name="Twitter",
-                            value=twitter
-                        )
-
-                        if github_response['hireable'] == True: hire = "Yes!"
-                        else: hire = "Not currently!"
-                        staff.add_field(
-                            name="Open to commissions?",
-                            value=hire
-                        )
-
-                        if github_response['blog'] is None or len(github_response['blog']) == 0 or github_response['blog'] == "":
-                            site = "No Website on GitHub!"
-                        else: site = github_response['blog']
-                        staff.add_field(
-                            name="Website",
-                            value=site
-                        )
-
-                        if github_response['company'] is not None:
-                            if github_response['company'].startswith("@"):
-                                try: company = f'[{github_response["company"].replace("@", "")}](https://github.com/{github_response["company"].replace("@", "")})'
-                                except: company = github_response['company']
-                            else:
-                                company = github_response['company']
-                        else: company = "No Company on GitHub!"
-                        staff.add_field(
-                            name="Company",
-                            value=company
-                        )
-
-                    staff.add_field(
-                        name="Position",
-                        value="\n".join(positions)
-                    )
-                    staff.add_field(
-                        name="System Access",
-                        value="\n".join(sysaccess)
-                    )
-
-                    staff.set_footer(
-                        icon_url=self.bot.user.avatar_url,
-                        text="IsThicc Staff"
-                    )
-                    request.close()
-                    github_r.close()
-
-                    return await msg.edit(embed=staff)
-            except Exception as e: print(e)
+                return await msg.edit(embed=staff)
 
             if code == 403:
                 request.close()
