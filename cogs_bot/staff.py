5dd14368b1ebf14a68e048c893e8eca7e1a32b45
Mrmagicpie <realmrmagicpie@gmail.com>
2021-02-15 19:31:07 -0500

    Update staff.py

--- cogs_bot/staff.py
+++ cogs_bot/staff.py
@@ -163,9 +163,9 @@ class staff_cog(commands.Cog):
                             value=hire
                         )
 
-                        if github_response['blog'] is not None or len(github_response['blog']) == 0 or github_response['blog'] == "":
-                            site = github_response['blog']
-                        else: site = "No Website on GitHub!"
+                        if github_response['blog'] is None or len(github_response['blog']) == 0 or github_response['blog'] == "":
+                            site = "No Website on GitHub!"
+                        else: site = github_response['blog']
                         staff.add_field(
                             name="Website",
                             value=site
