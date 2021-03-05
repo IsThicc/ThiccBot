139863b2adda6afd8321b58cbebba17f651762de
Mrmagicpie <realmrmagicpie@gmail.com>
2021-03-05 09:38:04 -0500

    Update starboard.py

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -1,27 +1,43 @@
+#
+#                          IsThicc-bot Starboard.py | 2020-2021 (c) IsThicc
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#
 import discord
 from discord.ext import commands
-from typing import Optional
-from datetime import datetime as d
-
+from typing      import Optional
+from datetime    import datetime as d
+#
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#
 class Starboard(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     
     async def __get_message(self, payload: discord.RawReactionActionEvent) -> Optional[discord.Message]:
+
         starboard = self.bot.get_channel(816095466067722250)
-        channel = self.bot.get_channel(payload.channel_id)
-        msg = await channel.fetch_message(payload.message_id)
+        channel   = self.bot.get_channel(payload.channel_id)
+        msg       = await channel.fetch_message(payload.message_id)
 
         async for message in starboard.history():
             if str(msg.id) in message.content:
                 return message
         
         return None
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     
     async def __update_starboard(self, count: int, payload: discord.RawReactionActionEvent):
-        channel = self.bot.get_channel(payload.channel_id)
+
+        channel   = self.bot.get_channel(payload.channel_id)
         starboard = self.bot.get_channel(816095466067722250)
-        msg = await channel.fetch_message(payload.message_id)
+        msg       = await channel.fetch_message(payload.message_id)
 
         async for message in starboard.history():
             if str(msg.id) in message.content:
@@ -30,16 +46,19 @@ class Starboard(commands.Cog):
                     await message.pin(reason='Starboard message over 10 reactions.')
                 break
 
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        user = self.bot.get_user(payload.user_id)
-        channel = self.bot.get_channel(payload.channel_id)
+
+        user      = self.bot.get_user(payload.user_id)
+        channel   = self.bot.get_channel(payload.channel_id)
         starboard = self.bot.get_channel(816095466067722250)
-        msg = await channel.fetch_message(payload.message_id)
+        msg       = await channel.fetch_message(payload.message_id)
 
-        if user.bot or payload.guild_id is None: return # so no screech
+        if user.bot or payload.guild_id is None: return  # so no screech
 
-        if str(payload.emoji) != '⭐': return # id rather not star every reacted message lmfao
+        if str(payload.emoji) != '⭐': return  # id rather not star every reacted message lmfao
 
         for reaction in msg.reactions:
             if str(reaction) == '⭐':
@@ -59,13 +78,15 @@ class Starboard(commands.Cog):
 
                 await starboard.send(f"⭐ **{reactions}** {channel.mention} ID: {msg.id}", embed=embed)
 
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+
         channel = self.bot.get_channel(payload.channel_id)
-        user = self.bot.get_user(payload.user_id)
+        user    = self.bot.get_user(payload.user_id)
 
         if user.bot is None: return
-
         if str(payload.emoji) != '⭐': return
 
         msg = await channel.fetch_message(payload.message_id)
@@ -79,5 +100,11 @@ class Starboard(commands.Cog):
 
         message = await self.__get_message(payload)
         return await message.delete()
+
+#
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#
 def setup(bot):
     bot.add_cog(Starboard(bot))
