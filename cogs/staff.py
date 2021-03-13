3b6f0d44e8b579c3120f63b62e2c27d772206c73
Mrmagicpie <realmrmagicpie@gmail.com>
2021-03-13 16:25:24 -0500

    Update staff.py

--- cogs/staff.py
+++ cogs/staff.py
@@ -26,7 +26,7 @@ class staff_cog(commands.Cog):
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 #
-    @commands.command()
+    @commands.group(name="staff")
     @commands.cooldown(1, 1, BucketType.user)
     @commands.has_role(744012353808498808)
     async def staff(self, ctx, option=None, member: str = None):
@@ -48,182 +48,10 @@ class staff_cog(commands.Cog):
 
             option = option.lower()
 
-            if option == "view":
-                if member is None:
-                    return await ctx.send(embed=em(
-                        title="You didn't supply a member!",
-                        description="Oh no, you forgot to supply a member to check! Make sure you do that next time!",
-                        colour=discord.Colour.red(),
-                        timestamp=datetime.utcnow()
-                    ).set_footer(
-                        icon_url=self.bot.user.avatar_url,
-                        text="IsThicc Staff"
-                    ))
-                else:
+            # if option == "view":
 
-                    discord_member = discord.utils.get(ctx.guild.members, name=member.split("#")[0])
-                    
-                    if discord_member is not None:
-                        msg = await ctx.send(embed=em(
-                            title=f"Attempting to view: {discord_member.display_name}",
-                            colour=discord.Colour.green(),
-                            timestamp=datetime.utcnow()
-                        ).set_footer(
-                            icon_url=self.bot.user.avatar_url,
-                            text="IsThicc Staff"
-                        ))
-                        member = str(discord_member.id)
 
-                    if member.startswith("<@"):
-                        member = re.search(r'(?<=<[!|@]|@!)\d+(?=>)', member)[0]
-
-                    msg = await ctx.send(embed=em(
-                        title=f"Attempting to view: {member}",
-                        colour=discord.Colour.green(),
-                        timestamp=datetime.utcnow()
-                    ).set_footer(
-                        icon_url=self.bot.user.avatar_url,
-                        text="IsThicc Staff"
-                    ))
-                  
-                    request = await self.session.get(f"http://10.42.10.4:5000/staff/{member}")
-                    code = request.status
-
-                    if code == 200:
-
-                        response = await request.json()
-                        # github_r = await self.session.get(f"https://api.github.com/users/{response['details']['github_username']}")
-                        # github_code = github_r.status
-                        await asyncio.sleep(2)
-
-                        positions = []
-                        for position in response['details']['position']:
-                            positions.append(f'- {position}')
-
-                        github = []
-                        for access in response['details']['github_access']:
-                            github.append(f'- {access}')
-
-                        sysaccess = []
-                        for access in response['details']['system_access']:
-                            sysaccess.append(f'- {access}')
-
-                        pronouns = []
-
-                        if len(response['details']['pronouns']) == 0:
-                            pronouns.append("Not specified.")
-
-                        for pronoun in response['details']['pronouns']:
-                            pronouns.append(f'- {pronoun}')
-
-                        staff = em(
-                            title=f"Showing info for {response['details']['name'].capitalize()}",
-                            description=f"Processed file: **``{response['details']['name']}.yml``**",
-                            colour=discord.Colour.green(),
-                            timestamp=datetime.utcnow()
-                        )
-                        staff.set_thumbnail(
-                            url=self.bot.get_user(response['details']['discord_id']).avatar_url
-                        )
-                        staff.add_field(
-                            name="VPN IP",
-                            value=response['details']['ip']
-                        )
-                        # staff.add_field(
-                        #     name="Twitter",
-                        #     value=twitter
-                        # )
-                        #
-                        # if github_response['hireable'] == True: hire = "Yes!"
-                        # else: hire = "Not currently!"
-                        # staff.add_field(
-                        #     name="Open to commissions?",
-                        #     value=hire
-                        # )
-                        #
-                        # if github_response['blog'] is None or len(github_response['blog']) == 0 or github_response['blog'] == "":
-                        #     site = "No Website on GitHub!"
-                        # else: site = github_response['blog']
-                        # staff.add_field(
-                        #     name="Website",
-                        #     value=site
-                        # )
-                        #
-                        # if github_response['company'] is not None:
-                        #     if github_response['company'].startswith("@"):
-                        #         try: company = f'[{github_response["company"].replace("@", "")}](https://github.com/{github_response["company"].replace("@", "")})'
-                        #         except: company = github_response['company']
-                        #     else:
-                        #         company = github_response['company']
-                        # else: company = "No Company on GitHub!"
-                        # staff.add_field(
-                        #     name="Company",
-                        #     value=company
-                        # )
-
-                        staff.add_field(
-                            name="Position",
-                            value="\n".join(positions)
-                        )
-                        staff.add_field(
-                            name="Pronouns",
-                            value="\n".join(pronouns)
-                        )
-                        staff.add_field(
-                            name="System Access",
-                            value="\n".join(sysaccess)
-                        )
-                        staff.add_field(
-                            name="GitHub Access",
-                            value="\n".join(github)
-                        )
-
-                        staff.set_footer(
-                            icon_url=self.bot.user.avatar_url,
-                            text="IsThicc Staff"
-                        )
-                        request.close()
-                        # github_r.close()
-
-                        return await msg.edit(embed=staff)
-
-                    elif code == 403:
-                        request.close()
-                        return await msg.edit(embed=em(
-                            title="Oh no!",
-                            description="You requested a staff member you're not allowed to access!",
-                            colour=discord.Colour.red(),
-                            timestamp=datetime.utcnow()
-                        ).set_footer(
-                            icon_url=self.bot.user.avatar_url,
-                            text="IsThicc Staff"
-                        ))
-
-                    elif code == 404:
-                        request.close()
-                        return await msg.edit(embed=em(
-                            title="Unknown Staff Member!",
-                            description="Your requested Staff Member does not exist!",
-                            colour=discord.Colour.red(),
-                            timestamp=datetime.utcnow()
-                        ).set_footer(
-                            icon_url=self.bot.user.avatar_url,
-                            text="IsThicc Staff"
-                        ))
-
-                    else:
-                        request.close()
-                        return await msg.edit(embed=em(
-                            title="U h",
-                            description=f"You ran into an unknown response code! Make sure to report this to the developers!\nExited with code `{code}`",
-                            colour=discord.Colour.dark_red(),
-                            timestamp=datetime.utcnow()
-                        ).set_footer(
-                            icon_url=self.bot.user.avatar_url,
-                            text="IsThicc Staff"
-                        ))
-
-            elif option == "edit":
+            if option == "edit":
                 return ctx.send(embed=em(
                     title="This option is under development!",
                     colour=discord.Colour.red(),
@@ -259,8 +87,187 @@ class staff_cog(commands.Cog):
             else:
                 return await ctx.send(embed=err_em)
 
-#
-#
+    #
+    #
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    @staff.command(name="view")
+    async def view(self, ctx, member: str = None):
+
+        if member is None:
+            return await ctx.send(embed=em(
+                title="You didn't supply a member!",
+                description="Oh no, you forgot to supply a member to check! Make sure you do that next time!",
+                colour=discord.Colour.red(),
+                timestamp=datetime.utcnow()
+            ).set_footer(
+                icon_url=self.bot.user.avatar_url,
+                text="IsThicc Staff"
+            ))
+        else:
+
+            discord_member = discord.utils.get(ctx.guild.members, name=member.split("#")[0])
+
+            if discord_member is not None:
+                msg = await ctx.send(embed=em(
+                    title=f"Attempting to view: {discord_member.display_name}",
+                    colour=discord.Colour.green(),
+                    timestamp=datetime.utcnow()
+                ).set_footer(
+                    icon_url=self.bot.user.avatar_url,
+                    text="IsThicc Staff"
+                ))
+                member = str(discord_member.id)
+
+            if member.startswith("<@"):
+                member = re.search(r'(?<=<[!|@]|@!)\d+(?=>)', member)[0]
+
+            msg = await ctx.send(embed=em(
+                title=f"Attempting to view: {member}",
+                colour=discord.Colour.green(),
+                timestamp=datetime.utcnow()
+            ).set_footer(
+                icon_url=self.bot.user.avatar_url,
+                text="IsThicc Staff"
+            ))
+
+            request = await self.session.get(f"http://10.42.10.4:5000/staff/{member}")
+            code = request.status
+
+            if code == 200:
+
+                response = await request.json()
+                # github_r = await self.session.get(f"https://api.github.com/users/{response['details']['github_username']}")
+                # github_code = github_r.status
+                await asyncio.sleep(2)
+
+                positions = []
+                for position in response['details']['position']:
+                    positions.append(f'- {position}')
+
+                github = []
+                for access in response['details']['github_access']:
+                    github.append(f'- {access}')
+
+                sysaccess = []
+                for access in response['details']['system_access']:
+                    sysaccess.append(f'- {access}')
+
+                pronouns = []
+
+                if len(response['details']['pronouns']) == 0:
+                    pronouns.append("Not specified.")
+
+                for pronoun in response['details']['pronouns']:
+                    pronouns.append(f'- {pronoun}')
+
+                staff = em(
+                    title=f"Showing info for {response['details']['name'].capitalize()}",
+                    description=f"Processed file: **``{response['details']['name']}.yml``**",
+                    colour=discord.Colour.green(),
+                    timestamp=datetime.utcnow()
+                )
+                staff.set_thumbnail(
+                    url=self.bot.get_user(response['details']['discord_id']).avatar_url
+                )
+                staff.add_field(
+                    name="VPN IP",
+                    value=response['details']['ip']
+                )
+                # staff.add_field(
+                #     name="Twitter",
+                #     value=twitter
+                # )
+                #
+                # if github_response['hireable'] == True: hire = "Yes!"
+                # else: hire = "Not currently!"
+                # staff.add_field(
+                #     name="Open to commissions?",
+                #     value=hire
+                # )
+                #
+                # if github_response['blog'] is None or len(github_response['blog']) == 0 or github_response['blog'] == "":
+                #     site = "No Website on GitHub!"
+                # else: site = github_response['blog']
+                # staff.add_field(
+                #     name="Website",
+                #     value=site
+                # )
+                #
+                # if github_response['company'] is not None:
+                #     if github_response['company'].startswith("@"):
+                #         try: company = f'[{github_response["company"].replace("@", "")}](https://github.com/{github_response["company"].replace("@", "")})'
+                #         except: company = github_response['company']
+                #     else:
+                #         company = github_response['company']
+                # else: company = "No Company on GitHub!"
+                # staff.add_field(
+                #     name="Company",
+                #     value=company
+                # )
+
+                staff.add_field(
+                    name="Position",
+                    value="\n".join(positions)
+                )
+                staff.add_field(
+                    name="Pronouns",
+                    value="\n".join(pronouns)
+                )
+                staff.add_field(
+                    name="System Access",
+                    value="\n".join(sysaccess)
+                )
+                staff.add_field(
+                    name="GitHub Access",
+                    value="\n".join(github)
+                )
+
+                staff.set_footer(
+                    icon_url=self.bot.user.avatar_url,
+                    text="IsThicc Staff"
+                )
+                request.close()
+                # github_r.close()
+
+                return await msg.edit(embed=staff)
+
+            elif code == 403:
+                request.close()
+                return await msg.edit(embed=em(
+                    title="Oh no!",
+                    description="You requested a staff member you're not allowed to access!",
+                    colour=discord.Colour.red(),
+                    timestamp=datetime.utcnow()
+                ).set_footer(
+                    icon_url=self.bot.user.avatar_url,
+                    text="IsThicc Staff"
+                ))
+
+            elif code == 404:
+                request.close()
+                return await msg.edit(embed=em(
+                    title="Unknown Staff Member!",
+                    description="Your requested Staff Member does not exist!",
+                    colour=discord.Colour.red(),
+                    timestamp=datetime.utcnow()
+                ).set_footer(
+                    icon_url=self.bot.user.avatar_url,
+                    text="IsThicc Staff"
+                ))
+
+            else:
+                request.close()
+                return await msg.edit(embed=em(
+                    title="U h",
+                    description=f"You ran into an unknown response code! Make sure to report this to the developers!\nExited with code `{code}`",
+                    colour=discord.Colour.dark_red(),
+                    timestamp=datetime.utcnow()
+                ).set_footer(
+                    icon_url=self.bot.user.avatar_url,
+                    text="IsThicc Staff"
+                ))
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 #
