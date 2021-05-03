4988a3f83141cde8177366bec58a42377d72a37d
Mrmagicpie <70033286+Mrmagicpie@users.noreply.github.com>
2021-05-03 16:20:36 -0400

    Update listener.py

--- cogs/listener.py
+++ cogs/listener.py
@@ -4,14 +4,13 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 #
-
 import discord, asyncio
 from discord.ext          import commands
 from discord.ext.commands import BucketType
 from discord              import Embed as em
 from datetime             import datetime
 from aiohttp              import ClientSession
-
+from datetime             import datetime as d, timedelta 
 #
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -22,34 +21,75 @@ channels = [
     838158218567614495 # test channel
 ]
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 class Listener(commands.Cog):
     def __init__(self, bot):
         self.bot     = bot
-        self.session  =  ClientSession()
+        self.session =  ClientSession()
+        
+        self.bot.verify_cache = {}
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id not in channels: return
-        if not message.author.bot: return await message.delete()
-        l = len(message.clean_content)
-        if l < 30: 
-            await message.author.send(content=f"Your message is {30-l} letters too short:\n```{message.content}```")
-            await message.delete()
-        elif l > 60:
-            await message.author.send(content=f"Your message is {l-60} letters too long:\n```{message.content}```")
-            await message.delete()
-
-    async def outline_rquest(self):
-        request = await self.session.post(f"uri")
-        code = request.status
-
-        if code == 403:
-            pass
-        elif code == 500:
-            pass
+        
+        if message.channel.id not in channels: 
+            return
+        
+        elif not message.author.bot: 
+            return await message.delete()
+        
+        id = message.author.id
+        c  = message.channel
+        
+        if id in self.bot.verify_cache:
+            if self.bot.verify_cache[id]['tries'] >= 4:
+                if not self.bot.verify_cache[id]['last_try'] >= (d.now() - timedelta(minutes=10)):
+                    return await c.send(embed=em(title="You are being ratelimited!", 
+                                                 description="Sorry! You have attempted to verify too many times! Please wait 10 minutes!",
+                                                 colour=discord.Colour.red()), delete_after=10)
+                
         else:
-            pass
+            self.bot.verify_cache[id]             = {}    
+            self.bot.verify_cache[id]['last_try'] = d.now()
+            self.bot.verify_cache[id]['tries']    = 0
+        
+        l = len(message.clean_content)
+        
+        if l < 30: 
+            await c.send(content=f"Your message is {30-l} letters too short:\n```{message.content}```")
+            await message.delete()
+        
+        elif l > 60:
+            await c.send(content=f"Your message is {l-60} letters too long:\n```{message.content}```")
+            await message.delete()
+            
+        else:
+            request = await self.session.post(f"http://127.0.0.1:2003/", headers={"Authorization": "fill in config token here", "AuthToken": message.clean_content})
+            code    = request.status
+
+            if code == 200:
+                return await c.send(embed=em(title="Verified!", 
+                                             colour=discord.Colour.green(),
+                                             description="You have been verified on the IsThicc Dashboard!"), delete_after=10)
+
+            if code == 403:
+                return await c.send(embed=em(title="Unable to verify!", 
+                                             description="Sorry! Your provided ID is not valid! Please make sure you copy the exact ID from [here](https://isthicc.dev/dash/discord).",
+                                             colour=discord.Colour.red()), delete_after=10)
+
+            else:
+                return await c.send(embed=em(title="An internal error has occurred!", 
+                                             description=f"Please let IsThicc management know that an internal error has occurred! Please provide the status code!\nStatus code: {code}",
+                                             colour=discord.Colour.red()), delete_after=10)
+            
+            request.close()
+        
+        self.bot.verify_cache[id]['tries']    += 1
+        self.bot.verify_cache[id]['last_try']  = d.now()
+        
 #
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
