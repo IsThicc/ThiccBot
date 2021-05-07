58aa49d0217f3389c7b347ac08d409ef578c0850
Fxcilities <fxcilities@deltanetmc.xyz>
2021-05-06 20:12:22 -0400

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -6,12 +6,13 @@
 #
 from discord.ext import commands
 from discord     import Embed, Color
+import random
 #
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 #
-class Ping(commands.Cog):
+class Other(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -23,21 +24,30 @@ class Ping(commands.Cog):
         ping = float(self.bot.latency * 1000)
         em   = Embed(title="IsThicc", color=Color.gold())
 
-        if ping <=20:
-            em.description = (f":green_circle: Latency: {ping:.0f}")
+        if ping <= 20:
+            em.description = f":green_circle: Latency: {ping:.0f}"
 
-        elif ping <=50:
-            em.description = (f":orange_circle: Latency: {ping:.0f}")
+        elif ping <= 50:
+            em.description = f":orange_circle: Latency: {ping:.0f}"
 
         else:
-            em.description = {f":red_circle: Latency: {ping:.0f}"}
+            em.description = f":red_circle: Latency: {ping:.0f}"
         
         return await ctx.send(embed=em)
+    
+    @commands.command()
+    async def mock(self, ctx, message: str = None):
+        if message is None:
+            return await ctx.send("pLeAsE sUpPlY A mESsAgE tO mOcK")
+        
+        list = [random.choice([s.upper(), s.lower()]) for s in message]
+        await ctx.send("".join(list))
 
 #
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 #
+
 def setup(bot):
-    bot.add_cog(Ping(bot))
+    bot.add_cog(Other(bot))
