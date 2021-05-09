f1fa7b5abea9b224657d8976116047d57a421742
Fxcilities <48619853+Fxcilities@users.noreply.github.com>
2021-05-09 19:41:02 -0400

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -5,7 +5,7 @@
 #
 #
 from discord.ext import commands
-from discord     import Embed, Color
+from discord     import Embed, Color, AllowedMentioned
 import random
 #
 #
@@ -44,7 +44,7 @@ class Other(commands.Cog):
             return await ctx.send("pLeAsE sUpPlY A mESsAgE tO mOcK")
         
         list = [random.choice([s.upper(), s.lower()]) for s in message]
-        await ctx.send("".join(list))
+        await ctx.send("".join(list), allowed_mentions=AllowedMentions(roles=False, members=False, everyone=False))
 
 #
 #
