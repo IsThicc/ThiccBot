2453174a2a9e5e5ff36232567357a9fcf84e428c
Mrmagicpie <realmrmagicpie@gmail.com>
2022-01-21 10:15:31 -0500

    update sql

--- database.sql
+++ database.sql
@@ -1,33 +1,35 @@
 
 -- TODO: Make all the old table names/column names better following camelCase
 CREATE TABLE IF NOT EXISTS tags (
-    name VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL PRIMARY KEY,
-    content VARCHAR(150) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
-    owner BIGINT NOT NULL,
+    name       VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL PRIMARY KEY,
+    owner      BIGINT NOT NULL,
+    content    VARCHAR(150) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
     command_id BIGINT NOT NULL,
     createdate DATETIME
 );
 
 CREATE TABLE IF NOT EXISTS tickets (
-    channel_id BIGINT NOT NULL PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    open BOOLEAN NOT NULL
+    open       BOOLEAN NOT NULL,
+    user_id    BIGINT  NOT NULL,
+    channel_id BIGINT  NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS warnings (
-    warn_id VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL PRIMARY KEY,
+    warn_id VARCHAR(30)
+        CHARACTER SET utf8 COLLATE utf8_swedish_ci
+                   NOT NULL PRIMARY KEY,
     user_id BIGINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Counting (
-    LastUser  BIGINT NOT NULL,
     Count     INT    NOT NULL DEFAULT 0,
+    LastUser  BIGINT NOT NULL,
     ChannelID BIGINT NOT NULL
 );
 
-
 CREATE TABLE IF NOT EXISTS Alphabet (
-    LastUser  BIGINT     NOT NULL,
-    Letter    VARCHAR(1) NOT NULL DEFAULT 'a',
-    ChannelID BIGINT     NOT NULL
+    Mode      VARCHAR(20) NOT NULL DEFAULT 'A_Z',
+    Letter    VARCHAR(1)  NOT NULL DEFAULT 'a',
+    LastUser  BIGINT      NOT NULL,
+    ChannelID BIGINT      NOT NULL
 );
